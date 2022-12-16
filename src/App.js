fcb8d6317df2a942a46d6f646c4a566a22d953b2
MJKM <rhkrrbaudgg@naver.com>
2022-12-16 22:16:08 +0900

    ✨ add: 주석 추가

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import CanvasIndex from "./canvas/Index";
 import HandCard from "./components/handCard/HandCard";
 
 function App() {
+  // 라우터 적용하기
+  // 하나의 페이지로 여러가지 프로젝트 보여주기
   return (
     <div className="view">
       <HandCard />
       <CanvasIndex />
-      <div>test</div>
     </div>
   );
 }
