d02f662a7d5ef25648ab26f3f4703313f207615b
MJKM <rhkrrbaudgg@naver.com>
2023-01-02 23:44:28 +0900

    ✨ add: welcome 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
-import CanvasIndex from "./canvas/Index";
-import HandCard from "./components/handCard/HandCard";
+import Welcome from "./components/welcome/Welcome";
 
 function App() {
   // 라우터 적용하기
   // 하나의 페이지로 여러가지 프로젝트 보여주기
   return (
     <div className="view">
-      <HandCard />
-      <CanvasIndex />
+      <Welcome />
     </div>
   );
 }
