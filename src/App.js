2413abb3bd32dd633c2ae2bb0548013374dfffa7
MJKM <rhkrrbaudgg@naver.com>
2023-01-03 23:23:48 +0900

    ✨ add: r코드 수정

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import Welcome from "./components/welcome/Welcome";
+import Puzzle from "./puzzle/Puzzle";
 
 function App() {
   // 라우터 적용하기
   // 하나의 페이지로 여러가지 프로젝트 보여주기
   return (
     <div className="view">
-      <Welcome />
+      <Puzzle />
     </div>
   );
 }
